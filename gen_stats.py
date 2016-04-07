85a67ca3ce74adb1acfb53af6e254c3c1f9453d3
hirosh7 <hirosh7@gmail.com>
2016-04-06 22:51:10 -0600

    implemented state data parsing and test

--- gen_stats.py
+++ gen_stats.py
@@ -14,15 +14,72 @@ This data will be refined into a new CSV file which can be used by Tableau for d
 from utilities import csvtools
 
 
-def build_gov2id_lookup(in_file):
+def _row2dictitem(in_row):
+    """
+    Process a state revenue data row The standard format is as follows:
+
+    Field Name	    Position	Start Position	Length	Characteristic
+    ------------------------------------------------------------------
+    Government ID 	     1-14	     1	            14	    Numeric,zero filled
+    Item Code           15-17	    15	             3	    Alpha numeric
+    Amount (thousands)	18-29	    18	            12	    Numeric
+    Survey Year	        30-31	    30	             2	    Numeric
+    Year of Data	    32-33	    32	             2	    Numeric
+    Origin	            34-35	    34	             2	    Alpha Numeric
+
+    **Note: Actual length of data appears to be 38 characters vs. 35 with the additional three characters belonging
+            to the amount data item (i.e. it appears to be 15 characters not 12)
+
+    :param in_row: a row of state data
+    :return: dictionary [field name: value]
+    """
+
+    out_dict = {'ID': in_row[:14], 'Item Code': in_row[14:17], 'Amount': in_row[18:32].strip(), 'Survey Year': in_row[32:34],
+                'Year of Data': in_row[34:36], 'Origin': in_row[36:38]}
+
+    return out_dict
+
+
+def process_state_data(in_file):
+    """
+    Process a state revenue file. Parse all fields into a dictionary
+
+    :param in_file: state revenue file (text)
+    :return: data dictionary [field name: value]
+    """
+
+    out_dict = {}
+
+    fh = open(in_file)
+    st_data = fh.read()
+
+    # create a list, one row = one list item
+    st_data_list = st_data.split('\n')
+
+    # break row data up into the appropriate
+    row_cnt = 1
+    for row in st_data_list:
+
+        print len(row)
+        # add itemized row data
+        out_dict[row_cnt] = _row2dictitem(row)
+
+        row_cnt += 1
+
+    return out_dict
+
+
+def build_lookup(in_file, key_fld):
+
     """
     Take the official government name and ID table and convert it into a dictionary with the ID as the key and
     state government name as the value
     :param in_file: government name/ID text file
-    :return: lookup dictionary [ID:gov_name]
+    :param key_fld: dictionary key field
+    :return: lookup dictionary [key_fld:row data]
     """
 
-    csv_dict = csvtools.csv2dict(in_file, 'ID Code')
+    csv_dict = csvtools.csv2dict(in_file, key_fld)
 
     return csv_dict
 
@@ -30,6 +87,12 @@ if __name__ == "__main__":
 
     # data files
     gov_id_file = 'data/government_ids.csv'
+    gov_itemcode_file = 'data/itemcodes.csv'
+    state_data_file = 'data/13state35.txt'
 
     # set up lookup tables
-    gov_id_lookup = build_gov2id_lookup(gov_id_file)
+    gov_id_lookup = build_lookup(gov_id_file, 'ID Code')
+    gov_itemcode_lookup = build_lookup(gov_itemcode_file, 'Item Code')
+
+    # get state data
+    state_data = process_state_data(state_data_file)
--- tests/test_gen_stats.py
+++ tests/test_gen_stats.py
@@ -1,8 +1,22 @@
 from unittest import TestCase
 import gen_stats
 
+
 class TestGen_Stats(TestCase):
 
     def test_build_gov2id_lookup(self):
-        lu_tab = gen_stats.build_gov2id_lookup('../data/government_ids.csv')
+        lu_tab = gen_stats.build_lookup('../data/government_ids.csv', 'ID Code')
         self.assertEqual(lu_tab['06000000000000']['State'].strip(), 'Colorado')
+
+    def test_row2dictitem(self):
+        row = '0000000000000019U      757843061131324'
+        row_items = gen_stats._row2dictitem(row)
+        self.assertEqual(row_items['ID'], '00000000000000')
+        self.assertEqual(row_items['Item Code'], '19U')
+        self.assertEqual(row_items['Amount'], '757843061')
+        self.assertEqual(row_items['Survey Year'], '13')
+        self.assertEqual(row_items['Year of Data'], '13')
+        self.assertEqual(row_items['Origin'], '24')
+
+
+
